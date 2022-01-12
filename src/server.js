dd26a2cab7387d470e354b38611be8a0f67fb6b5
minh1622 <huytranngoc098@gmail.com>
2022-01-12 15:16:12 +0700

    create server and project structure

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+var port = process.env.PORT || 3000
+
+app.get("/", (req, res)=>{
+    res.send("OKE server");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listen on port ${port}`)
+})
