94eef1934dda5f8ecca28eb3c1d1fe209536b0c3
minh1622 <huytranngoc098@gmail.com>
2022-02-15 01:00:32 +0700

    new logic for students model

--- src/models/students.model.js
+++ src/models/students.model.js
@@ -4,19 +4,24 @@ const mysql = require('mysql');
 
 const Students = function(student){
     this.id = student.id;
-    this.name = student.name;
     this.class = student.class;
-    this.address = student.address;
-    this.phone = student.phone;
-    this.email = student.email;
-    this.password = student.password;
 }
 
 Students.getById = function(id, result){
-    db.query(`SELECT * FROM students WHERE ID=${mysql.escape(id)}`, function(err, student){
+    db.query(`SELECT * FROM students WHERE userID=${mysql.escape(id)}`, function(err, student){
         if(err){
             result(null);
-        }else result(student);
+        }
+        else {
+            db.query(`SELECT * FROM users WHERE ID=${mysql.escape(student[0].userID)}`, (err, infoStudent) => {
+                if(err) console.log(err)
+                else {
+                    infoStudent[0]['class'] = student[0].class;
+                    delete infoStudent[0]['password'];
+                    result(infoStudent[0]);
+                }
+            })
+        }
     })
 }
 
